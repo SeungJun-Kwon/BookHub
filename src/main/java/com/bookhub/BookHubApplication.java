ff9d60267ce6c7e7f4a0e02cba76c6f2d9e1b318
DAchoMI <dachomi97@gmail.com>
2024-11-19 23:13:08 +0900

    :truck:

--- src/main/java/com/bookhub/BookHubApplication.java
+++ src/main/java/com/bookhub/BookHubApplication.java
@@ -1,4 +1,4 @@
-package com.bookhub.bookhub;
+package com.bookhub;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/com/bookhub/BookHubApplicationTests.java
+++ src/test/java/com/bookhub/BookHubApplicationTests.java
@@ -1,4 +1,4 @@
-package com.bookhub.bookhub;
+package com.bookhub;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
