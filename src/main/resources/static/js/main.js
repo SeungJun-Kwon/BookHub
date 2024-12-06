1f9bfaea2c7141a0b0f4dd21860ddb19f326f55c
DAchoMI <dachomi97@gmail.com>
2024-12-06 23:46:05 +0900

    :lipstick: jQuery 로 비동기 API 호출 및 데이터 출력

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -1,8 +1,19 @@
 $(function () {
-    
-    $("#user-table-body").append(`
-    <tr>
-        <td>123</td>
-    </tr>
-    `)
+    getAllUsers();
 });
+
+function getAllUsers() {
+    $.get("/users", function(result) {
+        let html;
+        $.each(result, function(i, v) {
+            html += `<tr>\n`;
+            html += `<td>${v.userId}</td>\n`
+            html += `<td>${v.userName}</td>\n`
+            html += `<td>${v.createdAt}</td>\n`
+            html += `<td>${v.modifiedAt}</td>\n`
+            html += `</tr>\n`
+        })
+
+        $("#user-table-body").append(html);
+    })
+}
